6069038b61239e9040f424c577efad581b0d7c39
Oscar Liljefors <oscar.lf@live.se>
2025-03-27 12:01:32 +0100

    started quiz, adding js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const formQuestions = Array.from(document.getElementsByClassName('form-questions'))
+const arrayOfQuestions = []
+
+
+//formQuestions.forEach((question) => {
+//  arrayOfQuestions.push(question)
+//})
+console.log(formQuestions)

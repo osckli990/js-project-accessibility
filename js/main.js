50067b1c13aa4229595d9ed8598d7cd803dc42ff
MalLunBar <malinelundgren1991@gmail.com>
2025-03-27 14:45:17 +0100

    Add a nextbutton function to change what question is visable

--- js/main.js
+++ js/main.js
@@ -1,8 +1,47 @@
 const formQuestions = Array.from(document.getElementsByClassName('form-questions'))
-const arrayOfQuestions = []
+const nextButton = document.getElementById('next-button')
 
-
-//formQuestions.forEach((question) => {
-//  arrayOfQuestions.push(question)
-//})
 console.log(formQuestions)
+
+
+let index = 0;
+
+
+
+
+const showNextQuestion = (i) => {
+
+  if (i >= (formQuestions.length - 1)) {
+    console.log("här borde vi sluta")
+  } else {
+
+    const currentQuestion = formQuestions[i]
+    const nextQuestion = formQuestions[(i + 1) % formQuestions.length]
+    console.log(nextQuestion)
+
+    currentQuestion.setAttribute('hidden', true)
+    nextQuestion.removeAttribute('hidden', false)
+
+
+    index += 1
+
+    console.log(index)
+
+  }
+
+  //tar in index och kollar vad det är? 
+  //lägga till 1 på indexet
+
+  //ändra så index variabel blir det nya numret
+
+  //visa array-item med det indexet
+
+  //ändra attributet till hidden eller ta bort hidden 
+
+}
+
+
+nextButton.addEventListener(('click'), () => {
+  showNextQuestion(index)
+})
+

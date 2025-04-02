7448302589b68241274c5c31b31625e36d40b9b8
Oscar Liljefors <oscar.lf@live.se>
2025-04-02 22:06:13 +0200

    fixed progress bar, ability to retake quiz and reset all inputs, changed layout back to one question at a time without animation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+//Question-cards
 const formQuestions = Array.from(document.getElementsByClassName('form-questions'))
+let index = 0
 
 //Buttons
-
 const resultButton = document.getElementById('result-button')
 const retakeButton = document.getElementById('retake-button')
 const resultsPage = document.getElementById('results')
 const bar = document.getElementById('bar')
 const percent = document.getElementById('percent-bar')
 const continueToQuiz = document.getElementById('intro-continue')
+const backButton = document.getElementById('previous-button')
+const nextButton = document.getElementById('next-button')
+//const startQuiz = document.getElementById('start-quiz')
 
 const announcer = document.getElementById('announcer')
 
@@ -19,10 +23,13 @@ const userInfoSection = document.getElementById('user-info')
 const userInfoForm = document.getElementById('user-info-form');
 const quizForm = document.getElementById('quiz')
 
+//progress bar
+const progressFill = document.querySelector('.progress-fill');
+const progressText = document.querySelector('.progress-text');
 
 
+const pointTotal = 4
 
-// let index = 0;
 //When we click continue to quiz
 continueToQuiz.addEventListener('click', () => {
 
@@ -33,88 +40,83 @@ continueToQuiz.addEventListener('click', () => {
   announcer.textContent = 'Moved to user information section';
 
 })
-//When we click start quiz
-
+//When we click continue to quiz
 
 userInfoForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
+  const currentQuestion = formQuestions[0]
+  currentQuestion.hidden = false
+
+  retakeButton.hidden = true
   quizSection.hidden = false
   userInfoSection.hidden = true
+  nextButton.hidden = false
 
 })
-
-
-
+//when we click start quiz
 
 //function that ensures the right buttons and questions are showing at the right time
-// const showNextQuestion = (i) => {
+const showNextQuestion = (i) => {
 
-//   if (i >= (formQuestions.length - 1)) {
-//     return
-//   } else if (i >= 0) {
+  if (i >= (formQuestions.length - 1)) {
+    return
+  } else if (i >= 0) {
 
-//     const currentQuestion = formQuestions[i]
-//     const nextQuestion = formQuestions[(i + 1) % formQuestions.length]
+    const currentQuestion = formQuestions[i]
+    const nextQuestion = formQuestions[(i + 1) % formQuestions.length]
 
-
-//     console.log(nextQuestion)
-
-//     backButton.removeAttribute('hidden')
-//     nextQuestion.removeAttribute('hidden')
-//     currentQuestion.setAttribute('hidden', true)
+    backButton.removeAttribute('hidden')
+    nextQuestion.removeAttribute('hidden')
+    currentQuestion.setAttribute('hidden', true)
 
 
-//     if (i === formQuestions.length - 2) {
-//       nextButton.setAttribute('hidden', true)
-//       resultButton.removeAttribute('hidden')
-//     }
+    if (i === formQuestions.length - 2) {
+      nextButton.setAttribute('hidden', true)
+      resultButton.removeAttribute('hidden')
+    }
 
-//     index += 1
-//     moveBar(index)
-//     console.log(index)
-//   }
-// }
+    index += 1
+  }
+}
 
-// const showPrevQuestion = (i) => {
+const showPrevQuestion = (i) => {
 
 
-//   if (i >= 1) {
-//     const currentQuestion = formQuestions[i]
-//     const prevQuestion = formQuestions[(i - 1) % formQuestions.length]
+  if (i >= 1) {
+    const currentQuestion = formQuestions[i]
+    const prevQuestion = formQuestions[(i - 1) % formQuestions.length]
 
+    currentQuestion.setAttribute('hidden', true)
+    prevQuestion.removeAttribute('hidden')
 
-//     //nextQuestion.classList.add('slide-out-right')
-//     //prevQuestion.classList.add('slide-in-left')
+    if (i === formQuestions.length - 1) {
+      resultButton.setAttribute('hidden', true)
+      nextButton.removeAttribute('hidden')
+    }
+    if (i === 1) {
+      backButton.setAttribute('hidden', true)
+    }
+  }
 
-//     currentQuestion.setAttribute('hidden', true)
-//     prevQuestion.removeAttribute('hidden')
+  index -= 1
 
-//     if (i === formQuestions.length - 1) {
-//       resultButton.setAttribute('hidden', true)
-//       nextButton.removeAttribute('hidden')
-//     }
-//     if (i === 1) {
-//       backButton.setAttribute('hidden', true)
-//     }
-//   }
-
-//   index -= 1
-
-// }
+}
 
 const countScore = () => {
   let points = document.querySelectorAll('input[value="true"]:checked')
   console.log("here is:", points)
   return points.length
+}
 
+const QuestionCount = () => {
+  let points = document.querySelectorAll('input[value="false"]:checked')
+  return points.length
 }
 
 const wrongQuestions = () => {
   let questions = document.querySelectorAll('input[value="false"]:checked')
   let question = []
-  //let falseQuestions = questions.closest(':not(span)').data('id')
-  //let falseQuestions = questions.parentElement
 
   let falseQuestions = []
   questions.forEach(question => {
@@ -128,27 +130,40 @@ const wrongQuestions = () => {
   return question
 }
 
+const updateProgress = () => {
+  let points = (countScore() + QuestionCount())
+  const percentage = (points / pointTotal) * 100;
+  console.log(percentage)
 
-const showResults = () => {
+  progressFill.style.width = `${percentage}%`;
+  progressText.textContent = `${points} of ${pointTotal} sections completed`;
+  announcer.textContent = `${points} of ${pointTotal} sections completed`;
+}
+
+quizForm.querySelectorAll('input[type="radio"]').forEach((radio) => {
+  radio.addEventListener('change', () => {
+    updateProgress()
+  })
+})
+//each input has an event listener cuasing a update
+
+
+const showResults = (i) => {
   const score = document.getElementById('score')
   const flunked = document.getElementById('flunked')
   const resultsMessage = document.getElementById('results-message')
-  //const currentQuestion = formQuestions[i]
-  const pointTotal = 4
+  const currentQuestion = formQuestions[i]
   let points = countScore()
   let questions = wrongQuestions()
 
-  //backButton.setAttribute('hidden', true)
-  //nextButton.setAttribute('hidden', true)
+  backButton.setAttribute('hidden', true)
+  nextButton.setAttribute('hidden', true)
   resultButton.setAttribute('hidden', true)
-  //currentQuestion.setAttribute('hidden', true)
+  currentQuestion.setAttribute('hidden', true)
   resultsPage.removeAttribute('hidden')
   retakeButton.removeAttribute('hidden')
 
   if (points < pointTotal) {
-    //flunked.innerHTML = `Questions you got wrong are: ${questions.forEach(q => {
-    //  `${q}`
-    //})}`
     flunked.innerHTML = `Questions you got wrong are: ${questions.map(q => { return q }).join(", ")}`
   } else {
     flunked.innerHTML = ''
@@ -169,24 +184,41 @@ const showResults = () => {
   }
 }
 
+nextButton.addEventListener(('click'), () => {
+  showNextQuestion(index)
+})
 
-
-
-
-
-
-
-//nextButton.addEventListener(('click'), () => {
-//  showNextQuestion(index)
-//})
-
-//backButton.addEventListener(('click'), () => {
-//  showPrevQuestion(index)
-//})
+backButton.addEventListener(('click'), () => {
+  showPrevQuestion(index)
+})
 
 quizForm.addEventListener(('submit'), (event) => {
   event.preventDefault()
-  showResults()
+  showResults(index)
 })
 
+retakeButton.addEventListener(('click'), () => {
+  resultsPage.hidden = true
+  index = 0
+  introSection.hidden = false
+  retakeButton.hidden = false
+  quizSection.hidden = true
+
+  let infoInput = [document.getElementById('name'), document.getElementById('email')]
+  infoInput.forEach((input) => {
+    input.value = ''
+  })
+  // clearing name and email input
+
+  document.querySelector('input[name="web-accessibility"]:checked').checked = false;
+  document.querySelector('input[name="WCAG"]:checked').checked = false;
+  document.querySelector('input[name="cognitive-disability"]:checked').checked = false;
+  document.querySelector('input[name="color-accessibility"]:checked').checked = false;
+  //clearing marked radio buttons
+
+  progressFill.style.width = `0%`
+  //clearing progress bar
+
+
+})
 //eventlistener där vi för "Retake quiz" knappen där vi ser till att den laddar om från quizet, inte introduction. 

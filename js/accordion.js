d06801e898db7504e9b5e2be1655c207d7d1f924
MalLunBar <malinelundgren1991@gmail.com>
2025-04-06 12:56:39 +0200

    styling for about us and eventlistener for button

---
+++ js/accordion.js
@@ -0,0 +1,7 @@
+const teamSection = document.getElementById('our-team')
+const teamButton = document.getElementById('team-button')
+
+
+teamButton.addEventListener('click', () => {
+  teamSection.hidden = false
+})
